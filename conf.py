03daab536ea8602ef16cacfcdb20c7910494f275
Fridolin Glatter <83776373+glatterf42@users.noreply.github.com>
2024-07-24 09:51:51 +0200

    Adjust to RTD's upcoming addon changes (#12)
    * Include config changes suggested by RTD
    
    * Remove duplicate workflow file

--- conf.py
+++ conf.py
@@ -10,11 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
 import datetime
+import os
 
 # -- Project information -----------------------------------------------------
 
@@ -38,7 +37,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: list[str] = []
 
 # -- Intersphinx -------------------------------------------------------------
 
@@ -68,6 +67,15 @@ html_static_path = ["_static"]
 html_css_files = [
     "iiasa.css",
 ]
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
 
 # prolog for all rst files
 rst_prolog = """
